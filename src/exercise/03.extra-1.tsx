3380a6ae220237929b053096be16f1ac98fb05c0
Matjah Sonneveld <matjah.sonneveld@blueconic.com>
2022-04-17 15:17:45 +0200

    feat: complete exercise 03 extra 01

---
+++ src/exercise/03.extra-1.tsx
@@ -0,0 +1,50 @@
+// Lifting state
+// http://localhost:3000/isolated/exercise/03.js
+
+import * as React from 'react'
+
+function Name() {
+  const [name, setName] = React.useState("");
+
+  return (
+    <div>
+      <label htmlFor="name">Name: </label>
+      <input id="name" value={name} onChange={event => setName(event.target.value)} />
+    </div>
+  )
+}
+
+function FavoriteAnimal({animal, onAnimalChange}: any) {
+  return (
+    <div>
+      <label htmlFor="animal">Favorite Animal: </label>
+      <input
+        id="animal"
+        value={animal}
+        onChange={onAnimalChange}
+      />
+    </div>
+  )
+}
+
+// function Display({name, animal}: any) {
+//   return <div>{`Hey ${name}, your favorite animal is: ${animal ? animal + "!" : ""}`}</div>
+// }
+
+function Display({animal}: any) {
+  return <div>{`Your favorite animal is: ${animal}!`}</div>
+}
+
+function App() {
+  const [animal, setAnimal] = React.useState("");
+
+  return (
+    <form>
+      <Name />
+      <FavoriteAnimal animal={animal} onAnimalChange={(event: any) => setAnimal(event.target.value)}/>
+      <Display animal={animal} />
+    </form>
+  )
+}
+
+export default App
