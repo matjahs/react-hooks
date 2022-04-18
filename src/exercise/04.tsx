05c3d88d405f04db2405e80d7299908113f89488
Matjah Sonneveld <matjah.sonneveld@blueconic.com>
2022-04-18 09:31:32 +0200

    feat: complete exercise 04

--- src/exercise/04.tsx
+++ src/exercise/04.tsx
@@ -1,41 +1,42 @@
 // useState: tic tac toe
 // http://localhost:3000/isolated/exercise/04.js
 
-import * as React from 'react'
+import * as React from "react";
+
+type Value = "X" | "O"
 
 function Board() {
-  // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
-
-  // 🐨 We'll need the following bits of derived state:
-  // - nextValue ('X' or 'O')
-  // - winner ('X', 'O', or null)
-  // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
+  const initialState = Array(9).fill(null);
+  const [squares, setSquares] = React.useState(initialState);
+  const [nextValue, setNextValue] = React.useState<Value>(calculateNextValue(squares));
+  const [winner, setWinner] = React.useState<Value | null>();
+  const [status, setStatus] = React.useState(calculateStatus(winner, squares, nextValue));
   // 💰 I've written the calculations for you! So you can use my utilities
   // below to create these variables
 
   // This is the function your square click handler will call. `square` should
   // be an index. So if they click the center square, this will be `4`.
-  function selectSquare(square: any) {
-    // 🐨 first, if there's already winner or there's already a value at the
-    // given square index (like someone clicked a square that's already been
-    // clicked), then return early so we don't make any state changes
-    //
-    // 🦉 It's typically a bad idea to mutate or directly change state in React.
-    // Doing so can lead to subtle bugs that can easily slip into production.
-    //
-    // 🐨 make a copy of the squares array
-    // 💰 `[...squares]` will do it!)
-    //
-    // 🐨 set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
-    //
-    // 🐨 set the squares to your copy
+  function selectSquare(square: number) {
+    if(square < 0 || square > 8) {
+      console.error(`square must be between 0 and 8, got ${square}`);
+      return;
+    }
+    if(squares[square] !== null) {
+      console.error(`square ${square} is already taken by ${squares[square]}`);
+      return;
+    }
+    if(winner) {
+      console.error(`game has ended, please restart to continue playing`);
+      return;
+    }
+
+    const squaresCopy = [...squares];
+    squaresCopy[square] = nextValue;
+    setSquares(squaresCopy);
   }
 
   function restart() {
-    // 🐨 reset the squares
-    // 💰 `Array(9).fill(null)` will do it!
+    setSquares(initialState);
   }
 
   function renderSquare(i: any) {
@@ -43,13 +44,23 @@ function Board() {
       <button className="square" onClick={() => selectSquare(i)}>
         {squares[i]}
       </button>
-    )
+    );
   }
+
+  React.useEffect(() => {
+    setWinner(calculateWinner(squares));
+    if(!winner) {
+      setNextValue(calculateNextValue(squares));
+    }
+  }, [squares, winner]);
+
+  React.useEffect(() => {
+    setStatus(calculateStatus(winner, squares, nextValue));
+  }, [winner, squares, nextValue]);
 
   return (
     <div>
-      {/* 🐨 put the status in the div below */}
-      <div className="status">STATUS</div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -69,7 +80,7 @@ function Board() {
         restart
       </button>
     </div>
-  )
+  );
 }
 
 function Game() {
@@ -79,26 +90,26 @@ function Game() {
         <Board />
       </div>
     </div>
-  )
+  );
 }
 
 // eslint-disable-next-line no-unused-vars
-function calculateStatus(winner: any, squares: any, nextValue: any) {
+function calculateStatus(winner: any, squares: any, nextValue: any): string {
   return winner
-    ? `Winner: ${winner}`
-    : squares.every(Boolean)
-    ? `Scratch: Cat's game`
-    : `Next player: ${nextValue}`
+         ? `Winner: ${winner}`
+         : squares.every(Boolean)
+           ? `Scratch: Cat's game`
+           : `Next player: ${nextValue}`;
 }
 
 // eslint-disable-next-line no-unused-vars
-function calculateNextValue(squares: any) {
-  return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O'
+function calculateNextValue(squares: any): Value {
+  return squares.filter(Boolean).length % 2 === 0 ? "X" : "O";
 }
 
 // eslint-disable-next-line no-unused-vars
-function calculateWinner(squares: any) {
-  const lines = [
+function calculateWinner(squares: any): Value | null {
+  const lines: number[][] = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -106,19 +117,19 @@ function calculateWinner(squares: any) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6],
-  ]
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i]
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a]
+    [2, 4, 6]
+  ];
+  for(let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
     }
   }
-  return null
+  return null;
 }
 
 function App() {
-  return <Game />
+  return <Game />;
 }
 
-export default App
+export default App;
